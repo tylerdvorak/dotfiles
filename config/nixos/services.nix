14754d4b11090781aebc6909738736b1802d9545
Tyler Dvorak <145394029+AWN-TDvorak@users.noreply.github.com>
2024-04-30 15:13:51 -0500

    Removed unneeded plasma stuff

--- config/nixos/services.nix
+++ config/nixos/services.nix
@@ -16,4 +16,9 @@
       desktopManager.plasma6.enable = true;
     };
   };
+  environment.plasma6.excludePackages = with pkgs.libsForQt5; [
+    plasma-browser-integration
+    konsole
+    oxygen
+  ];
 }
